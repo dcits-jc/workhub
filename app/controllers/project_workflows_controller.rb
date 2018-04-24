bef03077995cdedb4e6de350431b3559c019b5cf
liangchaob <liangchaob@163.com>
2018-04-24 16:35:02 +0800

    disable project cannot be commit

--- app/controllers/project_workflows_controller.rb
+++ app/controllers/project_workflows_controller.rb
@@ -12,54 +12,65 @@ class ProjectWorkflowsController < ApplicationController
   def create
     @project_workflow = ProjectWorkflow.new(project_workflow_params)
 
-    # 将终止时间+23.59小时
-    @project_workflow.end_time = @project_workflow.end_time+1.day-1.second
-    
-    # 如果用户成本没有,则设置为0
-    if current_user.cost.blank?
-      current_user.cost = 0
-      current_user.save
-    end
-
-    # 核算人天
-    @project_workflow.cost = current_user.cost * @project_workflow.hours / 8
-
-    # 两次','去重
-    itvendors = dry_tsstring(@project_workflow.other_itvendors)
-    # 如果这个厂商没找到,就新建这个厂商,最终加入隶属
-    itvendors.each do |v|
-      current_vendor = TagItvendor.find_by_name(v)
-      if current_vendor.blank?
-        current_vendor = TagItvendor.create(name: v)
+    @project = Project.find(project_workflow_params[:project_id])
+    if @project.enabled?
+      
+      # 将终止时间+23.59小时
+      @project_workflow.end_time = @project_workflow.end_time+1.day-1.second
+      
+      # 如果用户成本没有,则设置为0
+      if current_user.cost.blank?
+        current_user.cost = 0
+        current_user.save
       end
-      @project_workflow.join_tag_itvendors(current_vendor)
-    end
-    @project_workflow.other_itvendors = nil
 
-    # 两次','去重
-    itskills = dry_tsstring(@project_workflow.other_itskills)
-    # 如果这个技能没找到,就新建这个技能,最终加入隶属
-    itskills.each do |s|
-      current_skill = TagItskill.find_by_name(s)
-      if current_skill.blank?
-        current_skill = TagItskill.create(name: s)
+      # 核算人天
+      @project_workflow.cost = current_user.cost * @project_workflow.hours / 8
+
+      # 两次','去重
+      itvendors = dry_tsstring(@project_workflow.other_itvendors)
+      # 如果这个厂商没找到,就新建这个厂商,最终加入隶属
+      itvendors.each do |v|
+        current_vendor = TagItvendor.find_by_name(v)
+        if current_vendor.blank?
+          current_vendor = TagItvendor.create(name: v)
+        end
+        @project_workflow.join_tag_itvendors(current_vendor)
       end
-      @project_workflow.join_tag_itskills(current_skill)
-    end
-    @project_workflow.other_itskills = nil
+      @project_workflow.other_itvendors = nil
+
+      # 两次','去重
+      itskills = dry_tsstring(@project_workflow.other_itskills)
+      # 如果这个技能没找到,就新建这个技能,最终加入隶属
+      itskills.each do |s|
+        current_skill = TagItskill.find_by_name(s)
+        if current_skill.blank?
+          current_skill = TagItskill.create(name: s)
+        end
+        @project_workflow.join_tag_itskills(current_skill)
+      end
+      @project_workflow.other_itskills = nil
 
 
-    if @project_workflow.save
-      # 加入当前用户作为成员
-      @project_workflow.project.join!(current_user)
-      # 建立 feed 流
-      feed_create!(@project_workflow,current_user)
-      flash[:notice] = "提交成功!"
-      redirect_to root_path
+      if @project_workflow.save
+        # 加入当前用户作为成员
+        @project_workflow.project.join!(current_user)
+        # 建立 feed 流
+        feed_create!(@project_workflow,current_user)
+        flash[:notice] = "提交成功!"
+        redirect_to root_path
+      else
+        flash[:alert] = "提交失败!"
+        redirect_to root_path      
+      end
+
     else
-      flash[:alert] = "提交失败!"
-      redirect_to root_path      
+      flash[:alert] = "提交失败,该项目已被禁止继续报工!"
+      redirect_to root_path       
     end
+
+
+
   end
 
   private
