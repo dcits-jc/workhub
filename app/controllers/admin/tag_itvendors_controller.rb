b634b0d7f51d5436f197ad4cb221fae3f92ebc48
liangchaob <liangchaob@163.com>
2018-08-16 19:03:17 +0800

    add show to tags

--- app/controllers/admin/tag_itvendors_controller.rb
+++ app/controllers/admin/tag_itvendors_controller.rb
@@ -8,4 +8,10 @@ class Admin::TagItvendorsController < ApplicationController
     @tag_itvendors = @tag_itvendors.paginate(:page => params[:page], :per_page => 25)
   end
 
+  def show
+    @tag_itvendor = TagItvendor.includes(:project_workflows).find(params[:id])
+    @feeds = @tag_itvendor.project_workflows.map(&:feed).sort_by{|f| f.created_at}.reverse
+    @feeds = @feeds.paginate(:page => params[:page], :per_page => 20)
+  end
+
 end
