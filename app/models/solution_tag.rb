4121759cdd4081b30759e48d9ee08b9e6bba8dde
liangchaob <liangchaob@163.com>
2018-09-25 19:48:54 +0800

    fix scope

--- app/models/solution_tag.rb
+++ app/models/solution_tag.rb
@@ -3,8 +3,8 @@ class SolutionTag < ApplicationRecord
   has_many :solutionsolutiontages_relationships
   has_many :solutions, through: :solutionsolutiontages_relationships, source: :solution
 
-  scope :it_direction, -> { where("solution_type is '按技术方向'") }
-  scope :it_industry, -> { where("solution_type is '按行业'") }
+  scope :it_direction, -> { where(solution_type: '按技术方向') }
+  scope :it_industry, -> { where(solution_type: '按行业') }
 
 end
 
