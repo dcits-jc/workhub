f369b2d0bdd2ab209db16c4135df66a49b63d6b1
liangchaob <liangchaob@163.com>
2018-10-24 20:15:23 +0800

    rename download file name

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -143,7 +143,7 @@ class TeamsController < ApplicationController
     respond_to do |format|
       format.xls{ 
         # 设置文件名
-        headers["Content-Disposition"]="attachment; filename=部门周报("+@start_date.strftime('%Y-%m-%d')+"~"+@end_date.strftime('%Y-%m-%d')+").xls"
+        headers["Content-Disposition"]="attachment; filename=Daily("+@start_date.strftime('%Y-%m-%d')+"~"+@end_date.strftime('%Y-%m-%d')+").xls"
       }  
     end
 
