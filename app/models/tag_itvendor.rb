0641fec45476ac02201739b4d4a0a3ca5cf23f23
liangchaob <liangchaob@163.com>
2018-11-26 05:40:08 +0800

    add static order

--- app/models/tag_itvendor.rb
+++ app/models/tag_itvendor.rb
@@ -8,23 +8,22 @@ class TagItvendor < ApplicationRecord
   has_many :managementworkflowitvendor_relationships
   has_many :management_workflows, through: :managementworkflowitvendor_relationships, source: :management_workflow
 
-
   # n:m 熟悉 it 厂商的用户
   has_many :useritvendor_relationships
   has_many :users, through: :useritvendor_relationships, source: :user
 
   # 按照时间排序
   scope :order_by_created_at, -> { order("created_at ASC") }
-
   scope :order_by_commit_count, -> { includes(:project_workflows).sort_by{|t| t.commit_count}.reverse }
 
+  # 为了避免来回计算预设置的顺序
+  scope :order_by_staic, -> { where(id:[1,2,4,3,6,8,22,18,7,9,5]) }
 
   def total_hours
     sum = 0
     self.project_workflows.each {|p| sum+=p.hours}
     sum
   end
-
 
   def commit_count
     sum = 0
